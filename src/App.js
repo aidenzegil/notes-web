d846f69735b2673bfaf5f709eca8bda89991fff2
aidenzegil <40921524+aidenzegil@users.noreply.github.com>
2022-05-25 13:55:32 -0400

    all frontend functionality

--- src/App.js
+++ src/App.js
@@ -1,46 +1,68 @@
 import "./styling/App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import {postNote} from './utils/postNote'
 
 function App() {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [takingNote, setTakingNote] = useState(true);
-  const [selectedNote, setSelectedNote] = useState(null)
-  const testObject = { "test1" : "testBody1", "test2" : "testBody2", "test3" : "testBody3" };
+  const [nav, setNav] = useState([]);
+  const [selectedNote, setSelectedNote] = useState(null);
+  const testObject = {
+    ctest1: "testBody1",
+    btest2: "testBody2",
+    atest3: "testBody3",
+  };
 
   function getNotes() {
-  //fetch list of notes from backend
-  //will be called on start up and when new notes are submitted
+    //fetch list of notes from backend
+    //will be called on start up and when new notes are submitted
   }
-  
-  function handleNav() {
-    //takes in object from getNotes list
-    //creates a list from object keys using Object.keys(object)
-    //alphebetically organizes keys
-    //makes a button from each key that displays value
-    //set selected note to obj key that is clicked
-    //returns array of buttons
+
+  const handleClick = (e) => {
+    setSelectedNote(e.target.value);
+    setTakingNote(false);
+  };
+
+  function handleNav(navArray, navButtons = []) {
+    if (navButtons.length >= navArray.length) return setNav(navButtons);
+
+    const newNavButton = (
+      <button
+        value={navArray[navButtons.length]}
+        className="NavBtn"
+        onClick={handleClick}
+      >
+        {navArray[navButtons.length]}
+      </button>
+    );
+
+    return handleNav(navArray, [...navButtons, newNavButton]);
     //would this be better as a component?
   }
 
   function sendNote() {
-    //post title and body to backend
+    return postNote(title, body)
   }
-  console.log(Object.keys(testObject));
+
+  function handleSwitch() {
+    setTakingNote(true)
+    setSelectedNote(null);
+  }
+
+  useEffect(() => {
+    handleNav(Object.keys(testObject).sort());
+    console.log("useEffect");
+  }, []);
 
 
 
-
-
-
-
-
+  console.log(Object.keys(testObject).sort());
+  console.log(selectedNote);
   return (
     <div className="App">
       <div className="NavContainer">
-        <div className="Nav">
-
-        </div>
+        <div className="Nav">{nav}</div>
       </div>
       {takingNote ? (
         <div className="NoteContainer">
@@ -53,15 +75,17 @@ function App() {
           <header className="Header">Note:</header>
           <textarea
             className="Input"
+            style={{ height: 600 }}
             value={body}
             onChange={(e) => setBody(e.target.value)}
           />
+          <button style={{ width: "fit-content", fontSize: 30}}className="Add" onClick={sendNote}>Post</button>
         </div>
       ) : (
         <div className="NoteContainer">
-          <header className="Header">Title</header>
-          <body className="note"> This is where a note would be </body>
-          <button onClick={setTakingNote(true)}> + </button>
+          <header className="Header">{selectedNote}</header>
+          <body className="Header">{testObject[selectedNote]}</body>
+          <button className="Add" onClick={handleSwitch}> + </button>
         </div>
       )}
     </div>
